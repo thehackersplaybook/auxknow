92c00b1ee21ba927b0d0e5ac346b0d0c066142e5
Aditya Patange <contact.adityapatange@gmail.com>
2025-03-14 22:03:12 +0530

    update examples

---
+++ examples/streaming_fast_mode.py
@@ -0,0 +1,35 @@
+import sys
+from auxknow import AuxKnow
+from rich import print as rprint
+
+
+def main():
+    """AuxKnow example with streaming!"""
+    answer_engine = AuxKnow(verbose=True)
+    question = ""
+
+    while question.strip().lower() != "q" or question.strip().lower() != "quit":
+        question = input("💡 Enter a question for AuxKnow  (Press 'q' to exit): ")
+        if question.strip().lower() == "q" or question.strip().lower() == "quit":
+            break
+        response = answer_engine.ask_stream(question, fast_mode=True)
+
+        answer = "Answer: "
+        citations = ""
+        for partial_response in response:
+            if partial_response.answer and not partial_response.is_final:
+                answer += partial_response.answer
+                # Clear the line and print the current answer
+                sys.stdout.write(partial_response.answer)
+                sys.stdout.flush()
+            if partial_response.citations:
+                citations = partial_response.citations
+            if partial_response.is_final:
+                print()
+                break
+
+        rprint(f"[blue]Citations:[/blue] {citations}")
+
+
+if __name__ == "__main__":
+    main()
